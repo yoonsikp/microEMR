e73f19d338a50f4e5e4373e52173f30759a73159
Yoonsik Park <yoonsik@macbook-air-d5.local>
2022-11-03 14:21:01 -0500

    continue rename to upstream, remove old files

--- scripts/old/adduser.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# stop on all errors
-set -e
-
-cd ./ansible/
-ssh -p2222 localhost "sudo -S smbpasswd -a joel"
--- scripts/old/initgit.sh
+++
@@ -1,3 +0,0 @@
-cd ./A/data/
-git init --separate-git-dir ../git/
-git --git-dir=../git/ status
